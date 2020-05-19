1036969220c3c2bd86749742ea82d613468aa3b4
Wang, Alice <alice.wang@zeiss.com>
2020-05-19 13:43:50 +0800

    Initial version.

---
+++ WebServerWithLog4NET/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebServerWithLog4NET.Models;
+
+namespace WebServerWithLog4NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ILog log;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            log = LogManager.GetLogger(Startup.repository.Name, typeof(HomeController));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("logInfo")]
+        public JsonResult WriteLogInfo()
+        {
+            string info = "This is a info message.";
+            log.Info(info);
+            return Json("");
+        }
+
+        [Route("logError")]
+        public JsonResult WriteLogError()
+        {
+            string info = "This is a error message.";
+            log.Error(info);
+            return Json("");
+        }
+    }
+}
---
+++ WebServerWithLog4NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebServerWithLog4NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
